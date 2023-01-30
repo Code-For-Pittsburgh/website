c959e0a30c9c168742d8896984932cb9152a127c
Asilbec <asilbekomonkulov2003@gmail.com>
2023-01-30 02:32:40 -0500

    fixed next.js redirct issue and fixed read me

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Website
+# Code For Pittsburgh Website
 
+ 
+ ## Built using the latest : 
+ * Next.js
+ * Tailwind.css
+ * React
 
+## To-do : 
+        improve seo
--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
       <footer>
         <div className="flex flex-col md:flex-row gap-3 items-center justify-between py-10 max-w-screen-lg mx-auto text-sm px-5 text-gray-500">
           <p>
-            © 2023{" "}
+            © 2023
             <a
-              href="https://web3templates.com"
+              href="https://www.linkedin.com/company/code-for-pittsburgh/"
               target="_blank"
               rel="noopener">
               Code For Pittsburgh
@@ -15,22 +15,17 @@ export default function Footer() {
           </p>
           <nav className="flex gap-5">
             <a
-              href="http://"
+              href="https://www.linkedin.com/company/code-for-pittsburgh/"
               target="_blank"
               rel="noopener noreferrer">
               Terms
             </a>
+
             <a
-              href="http://"
+              href="https://www.linkedin.com/company/code-for-pittsburgh/"
               target="_blank"
               rel="noopener noreferrer">
               License
-            </a>
-            <a
-              href="http://"
-              target="_blank"
-              rel="noopener noreferrer">
-              FAQ
             </a>
           </nav>
         </div>
