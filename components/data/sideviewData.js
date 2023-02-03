7a1d66ce38ba06568e8b808596e6244692db2eb9
Derek <darmfield2023@gmail.com>
2023-02-03 14:16:08 -0500

    changed sideview look

--- components/data/sideviewData.js
+++ components/data/sideviewData.js
@@ -3,20 +3,12 @@ import test from "../../public/img/isotest.png";
 const Data = [
   {
     Label: "Why us?",
-    title: "Leave your problems to us",
+    title: "Tech is hard",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    buttonText: "Learn more",
+    "If you're into dealing with complicated frame works, hidden errors, and version control, then we're not for you. Let us handle the annoying code so you can focus on what you do best.",
+    buttonText: "Work with us",
     image: test
   },
-  {
-    Label: "Why us?",
-    title: "Leave your problems to us",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    buttonText: "Learn more",
-    image: test
-  }
 ];
 
 export default Data;
