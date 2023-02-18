b83114174a0f39fb6ef9ffc8bff4ec4e5de33b6d
Derek <darmfield2023@gmail.com>
2023-02-17 19:38:58 -0500

    updated stats

--- components/stats.js
+++ components/stats.js
@@ -6,10 +6,10 @@ export default function Stats() {
           <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
             <div className="flex flex-col items-center justify-center">
               <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-                15+ 
+               $2,500
               </dt>
               <dd className="font-light text-gray-500 dark:text-gray-400">
-                Years of experience
+                Provided in services
               </dd>
             </div>
             <div className="flex flex-col items-center justify-center">
