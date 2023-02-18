838f4a19e7ac21fc2247cee66134e987de7ead96
Derek <darmfield2023@gmail.com>
2023-02-17 23:18:37 -0500

    created data to modularly update partners

---
+++ components/data/partnerData.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+      name: "Taylor Allderdice Studnet-Led Newspaper",
+      date: "February 10th, 2023",
+      description: "With our hands tied, it was the only thing I codl resort to. Brute force. Frantically, I began typing as if my life depended on it, because it did. I knew that if i could not crack the secret code, not only me, but everyone I knew would be killed.",
+      src: "https://images.pexels.com/photos/3879060/pexels-photo-3879060.jpeg?cs=srgb&dl=pexels-alex-azabache-3879060.jpg&fm=jpg",
+      url: "www.goofyland.com"
+    },
+
+    {
+        name: "Next Partner",
+        date: "February 20th, 2023",
+        description: "",
+        src: "", 
+        url: ""
+      },
+  ];
+
+  
+  
+export default data;
+  
