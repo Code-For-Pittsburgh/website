df0665e678739232c763f3314969cc7224e6db54
Derek <darmfield2023@gmail.com>
2023-02-21 11:07:29 -0500

    removed and changed footer links

--- components/footer.js
+++ components/footer.js
@@ -46,7 +46,7 @@ export default function Footer() {
               <ul className="text-gray-600 dark:text-gray-400">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/themesberg/raphael2G"
+                    href="https://github.com/Code-For-Pittsburgh"
                     className="hover:underline ">
                     Github
                   </a>
@@ -86,12 +86,12 @@ export default function Footer() {
             <a
               href="https://flowbite.com"
               className="hover:underline">
-              Code for Pittsburgh™
+              Code for Pittsburgh.
             </a>
-            . All Rights Reserved.
+              &nbsp;All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a
+            {/* <a
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg
@@ -105,8 +105,8 @@ export default function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-            <a
+            </a> */}
+            {/* <a
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg
@@ -120,8 +120,8 @@ export default function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-            <a
+            </a> */}
+            {/* <a
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg
@@ -131,9 +131,9 @@ export default function Footer() {
                 aria-hidden="true">
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
-            </a>
+            </a> */}
             <a
-              href="#"
+              href="https://www.github.com/Code-for-Pittsburgh"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg
                 className="w-5 h-5"
@@ -147,7 +147,7 @@ export default function Footer() {
                 />
               </svg>
             </a>
-            <a
+            {/* <a
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg
@@ -161,7 +161,7 @@ export default function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
