23b185360f223130774ec609ae26a5446522f5ac
Derek <darmfield2023@gmail.com>
2023-02-21 10:28:56 -0500

    had partners page import from data

--- pages/partners.js
+++ pages/partners.js
@@ -3,12 +3,13 @@ import Footer from "@components/footer";
 
 // Page Specific Imports
 import Hero from "@components/partnerPage/hero";
+import TaylorAllderdice from "@components/partnerPage/taylorAllderdice";
 
 export default function Partners(){
     return(
         <>
             <Navbar />
-            <Hero />
+            <TaylorAllderdice />
             <Footer />
         </>
     )
