6d97e61493eeceaef7324787e81983b2eb3baaa6
Derek <darmfield2023@gmail.com>
2023-03-10 10:40:44 -0500

    updated text elements

--- components/landingPage/aboutus.js
+++ components/landingPage/aboutus.js
@@ -6,42 +6,46 @@ import {
 
 const features = [
   {
-    name: "Push to deploy.",
+    name: "Initial Meeting",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "During our first meeting, we'll talk about what you do, what you need and how long you can expect things to take.",
     icon: CloudArrowUpIcon
   },
   {
-    name: "SSL certificates.",
+    name: "Development",
     description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      "It's time for you to take it easy. We'll be busy building your website, carefully designing every feature so it's perfect for you. ",
     icon: LockClosedIcon
   },
   {
-    name: "Database backups.",
+    name: "Showcase",
     description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      "We'll meet for a second time and show you what we've made. Here, we'll take feedback and do a round of revisions with any new features.",
+    icon: ServerIcon
+  }, 
+  {
+    name: "Production",
+    description:
+      "After we make everything to your liking, we'll push to production. Take a look at our partners for what we've done before, but we can make everything to your liking!",
     icon: ServerIcon
   }
 ];
 
 export default function AboutUs() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
+    <div id='' className="overflow-hidden bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Deploy faster
+                Build fast
               </h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                A better workflow
+                A better setup
               </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Maiores impedit perferendis suscipit eaque, iste
-                dolor cupiditate blanditiis ratione.
+                Need a custom website, but don&apos;t know where to start? We&apos;re here to help. Take a look at our onboarding process. 
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map(feature => (
@@ -60,7 +64,7 @@ export default function AboutUs() {
             </div>
           </div>
           <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            src='img/taylorallderdiceLightTheme.png'
             alt="Product screenshot"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
