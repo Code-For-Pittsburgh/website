f526c27c669772c2d33d1540b934d43764f911b7
Derek <darmfield2023@gmail.com>
2023-03-10 10:40:20 -0500

    created underdevelopment template

---
+++ components/aboutPage/aboutSite.js
@@ -0,0 +1,9 @@
+export default function AboutSite() {
+    return(
+        <>
+            <div className="mx-auto my-96 text-center ">
+                This page is under development. Come back soon!
+            </div>
+        </>
+    );
+}
---
+++ components/companyPage/companySite.js
@@ -0,0 +1,11 @@
+
+export default function CompanySite(){
+    return(
+        <>
+            <div className="mx-auto my-96 text-center ">
+                This page is under development. Come back soon!
+            </div>
+        </>
+
+    );
+}
