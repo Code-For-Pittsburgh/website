a6dc9f06152ff79693cbe268d5a5abee78f35814
Derek <darmfield2023@gmail.com>
2023-03-10 10:41:28 -0500

    updated text elements

--- components/landingPage/smallbanner.js
+++ components/landingPage/smallbanner.js
@@ -37,19 +37,19 @@ export default function SmallBanner() {
       </svg>
       <div className="flex flex-wrap items-center gap-y-2 gap-x-4">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">GeneriCon 2023</strong>
+          <strong className="font-semibold">New 2023 Partners</strong>
           <svg
             viewBox="0 0 2 2"
             className="mx-2 inline h-0.5 w-0.5 fill-current"
             aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Join us in Denver from June 7 – 9 to see what’s coming next.
+          Join us in our mission to make a better Pittsburgh
         </p>
         <a
-          href="#"
+          href="/contact"
           className="flex-none rounded-full bg-gray-900 py-1 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
-          Register now <span aria-hidden="true">&rarr;</span>
+          Get started now! <span aria-hidden="true">&rarr;</span>
         </a>
       </div>
       <div className="flex flex-1 justify-end">
