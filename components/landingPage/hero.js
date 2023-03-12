ca8fab21c2b783178f3fd08d265a41bedbecad1c
D Baller <darmfield2023@gmail.com>
2023-03-12 01:01:36 -0500

    created announcemetns page

--- components/landingPage/hero.js
+++ components/landingPage/hero.js
@@ -19,7 +19,7 @@ export default function Hero() {
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Announcing our next cohort of partners. {" "}
-                <a href="#" className="font-semibold text-indigo-600">
+                <a href="/announcement" className="font-semibold text-indigo-600">
                   <span
                     className="absolute inset-0"
                     aria-hidden="true"
---
+++ pages/announcement.js
@@ -0,0 +1,14 @@
+import Navbar from "@components/navbar";
+import Footer from "@components/footer";
+
+export default function About(){
+    return(
+        <>
+            <Navbar />
+            <div className="text-center mx-auto my-96">
+                This post is comming soon, but just know we are planning something big. Check out our timeline on the main page for a hint as to what's coming. 
+            </div>
+            <Footer />
+        </>
+    )
+}
