cddd48d313d91440fd60ec5a4eaa0ef5eb197cca
--raphael <darmfield2023@gmail.com>
2023-03-12 22:12:17 -0400

    added mouse trailer to 404

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,12 @@
 import Navbar from "@components/navbar";
 import Footer from "@components/footer";
+import MouseTrailer from "@components/mouseTrailer/mouseTrailer";
 
 export default function MissingPage(){
     return(
         <>
             <Navbar />
+            <MouseTrailer/>
             <div className="mx-auto my-96 text-center">
                 <p>
                     Uh oh. We couldn&apos;t find this page. Check back soon!
