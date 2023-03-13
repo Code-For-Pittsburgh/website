a049f2b07b14163a6f3040a5034729e800cb6843
--raphael <darmfield2023@gmail.com>
2023-03-12 23:55:31 -0400

    added border to navBar

--- components/navbar.js
+++ components/navbar.js
@@ -65,7 +65,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
 
   return (
-    <header className="bg-white">
+      <header className="bg-white" style={{borderTop: "black 1px solid", borderBottom: "black 1px solid"}}>
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global">
