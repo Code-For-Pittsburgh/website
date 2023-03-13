cdbb593dce96ee7279b52e178d5dc455ef1b43aa
--raphael <darmfield2023@gmail.com>
2023-03-12 23:39:05 -0400

    replaced old favicon with pittsburgh favicon

--- components/navbar.js
+++ components/navbar.js
@@ -70,11 +70,11 @@ export default function Navbar() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+          <a href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">- - - - - - - - Code For Pittsburgh - - - - - - - </span>
             <img
               className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              src="/bridgeFavicon.svg"
               alt=""
             />
           </a>
